e57719d1ba4205e35c5605a3f13072d103cdfb7b
Dennis Furrer <dennisfurrer@pc-207-143.customer.iqgrove.quintain.lan>
2018-09-27 13:27:58 +0100

    Front-End: fetch GET /roars and append to page

--- client/client.js
+++ client/client.js
@@ -2,9 +2,13 @@ console.log('Hello World');
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const roarsElement = document.querySelector('.roars');
 const API_URL = 'http://localhost:5000/roars';
 
-loadingElement.style.display = 'none';
+loadingElement.style.display = '';
+
+
+listAllRoars();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -34,5 +38,32 @@ form.addEventListener('submit', (event) => {
         form.style.display = '';
 
     });
-
 });
+
+function listAllRoars() {
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(roars => {
+            console.log(roars);
+            roars.reverse();
+            roars.forEach(roar => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = roar.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = roar.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(roar.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                roarsElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none';
+        });
+}
